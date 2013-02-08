f7c0f7872970812876bb048b05e27ef74b9b08f0
Daniel Kalotay <daniel.kalotay@7digital.com>
2013-02-08 12:41:11 +0000

    addition is a collaborator

--- PocketCalculator/CasioCalculator.cs
+++ PocketCalculator/CasioCalculator.cs
@@ -2,58 +2,94 @@
 {
     public class CasioCalculator
     {
-        private decimal _numbers;
-        private decimal _buffer;
-        public string Display { get; private set; }
+        private decimal _display;
+        private IBuffer _buffer;
+        private decimal _input;
+
+        public string Display
+        {
+            get { return string.Format("{0}.", _display); }
+        }
 
         public void PressAC()
         {
-            _numbers = 0m;
-            GenerateDisplay(_numbers);
+            _display = 0m;
+            _input = 0m;
+            _buffer = new NullBuffer();
         }
 
         public void PressDigit(Digits digit)
         {
-            if (_numbers >= 999999999m) return;
+            if (_display >= 999999999m) return;
 
-            _numbers = _numbers * 10m + (byte) digit;
-            GenerateDisplay(_numbers);
-        }
-
-        private void GenerateDisplay(decimal numbers)
-        {
-            Display = string.Format("{0}.", numbers);
+            _input = _input * 10m + (byte) digit;
+            _display = _input;
         }
 
         public void PressEqual()
         {
-            _numbers += _buffer;
-            GenerateDisplay(_numbers);
-            _numbers = 0m;
-            _buffer = 0m;
+            _display = _buffer.ApplyTo(_display);
+            _buffer = new NullBuffer();
+            _input = 0m;
         }
 
         public void PressPlus()
         {
-            _buffer += _numbers;
-            GenerateDisplay(_buffer);
-            _numbers = 0m;
+            _display = _buffer.ApplyTo(_display);
+            _buffer = new AdditionBuffer();
+            _buffer.AddToBuffer(_display);
+            _input = 0m;
         }
 
         public void PressMinus()
         {
-            _numbers = 0m;
+            _display = 0m;
         }
 
         public void PressStar()
         {
-            _numbers = 0m;
+            _display = 0m;
         }
 
         public void PressSlash()
         {
-            _numbers = 0m;
+            _display = 0m;
         }
+    }
+
+    public class NullBuffer : IBuffer
+    {
+        public void AddToBuffer(decimal number)
+        {
+        }
+
+        public decimal ApplyTo(decimal number)
+        {
+            return number;
+        }
+    }
+
+    public class AdditionBuffer : IBuffer
+    {
+        private decimal _buffer;
+
+        public void AddToBuffer(decimal number)
+        {
+            _buffer = number;
+        }
+
+        public decimal ApplyTo(decimal number)
+        {
+            return number + _buffer;
+        }
+
+    }
+
+    internal interface IBuffer
+    {
+        void AddToBuffer(decimal number);
+
+        decimal ApplyTo(decimal number);
     }
 
     public enum Digits:byte
