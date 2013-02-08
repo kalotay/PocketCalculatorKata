639ad0603fb5bd43adf58788a0f375a32edb045b
Daniel Kalotay <daniel.kalotay@7digital.com>
2013-02-08 11:07:07 +0000

    setup project

---
+++ PocketCalculator/PocketCalculatorSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace PocketCalculator
+{
+    [Binding]
+    public class PocketCalculatorSteps
+    {
+        [Given(@"I have a pocket calculator")]
+        public void GivenIHaveAPocketCalculator()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I press ""(.*)""")]
+        public void WhenIPress(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the display shows ""(.*)""")]
+        public void ThenTheDisplayShows(Decimal p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
